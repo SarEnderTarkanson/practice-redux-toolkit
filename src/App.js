970375dc981c3af491b2db1ea59ba791e94f08d4
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-02-15 00:36:29 +0100

    fetched data from a mock api / created a layout with the help of bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Product from './components/Product';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Product from "./components/Product";
 
 function App() {
   return (
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const Product = () => {
   const [products, getProducts] = useState([]);
@@ -8,10 +10,31 @@ const Product = () => {
       .then((result) => getProducts(result));
   }, []);
 
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>INR: {product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ backgroundColor: "white" }}>
+          <Button variant="primary">Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
       <h1>Product Dashboard</h1>
-      {JSON.stringify(products)}
+      <div className="row">{cards}</div>
     </>
   );
 };
