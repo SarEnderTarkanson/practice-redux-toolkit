74df221b1f0ef68092a53e5c634265075c220d25
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-02-17 20:42:45 +0100

    consumed the global state in counting the products in the Cart / created remove action in our slice to remove items from the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,40 @@
+import { useSelector, useDispatch } from "react-redux";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { remove } from "../store/cartSlice";
+
 const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);
+  const removeFromCart = (id) => {
+    dispatch(remove(id));
+  };
+
+  const cards = products.map((product) => (
+    <div className="col-md-12" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>INR: {product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ backgroundColor: "white" }}>
+          <Button variant="danger" onClick={() => removeFromCart(product.id)}>
+            Remove Item
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
   return (
     <>
-      <h2>Cart</h2>
+      <div className="row">{cards}</div>
     </>
   );
 };
--- src/components/NavBarPanel.js
+++ src/components/NavBarPanel.js
@@ -2,13 +2,15 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NavBarPanel = () => {
+  const cartProducts = useSelector((state) => state.cart);
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
-        
+
         <Nav>
           <Nav.Link to="/" as={Link}>
             Products
@@ -16,11 +18,11 @@ const NavBarPanel = () => {
         </Nav>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
+          <Navbar.Text>
             <Nav.Link to="/cart" as={Link}>
-            My Bag: 0
-          </Nav.Link>
-            </Navbar.Text>
+              My Bag: {cartProducts.length}
+            </Nav.Link>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
